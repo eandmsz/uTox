62df7e55e7396d084cf53e25376265cdddcf73ba
Andreas <34283919+eandmsz@users.noreply.github.com>
2020-04-25 19:51:08 +0200

    fix Monoaki theme definition

--- src/main.c
+++ src/main.c
@@ -147,7 +147,7 @@ void parse_args(int argc, char *argv[],
                 } else if (!strcmp(optarg, "solarized-dark")) {
                     settings.theme = THEME_SOLARIZED_DARK;
                 } else if (!strcmp(optarg, "monokai")) {
-                    settings.theme = THEME_SOLARIZED_DARK;
+                    settings.theme = THEME_MONOKAI;
                 } else {
                     LOG_NORM("Please specify correct theme (please check user manual for list of correct values).\n");
                     exit(EXIT_FAILURE);
